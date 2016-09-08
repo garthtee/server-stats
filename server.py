cff45ecc1bbf20b63ef7a392d26fdeb598201637
Garth Toland <tolandgarth@gmail.com>
2016-09-08 13:30:32 +0100

    Moved server onto seperate thread

--- server.py
+++ server.py
@@ -3,22 +3,52 @@ import socketio
 import eventlet
 import eventlet.wsgi
 import json
+import time
+import threading
 from flask import Flask, render_template
+from gcm import GCM
+
 
 sio = socketio.Server()
 app = Flask(__name__)
+sid_array = []
+threads = []
+API_KEY = ''
+
+
+class Server (threading.Thread):
+    def __init__(self, thread_id):
+        threading.Thread.__init__(self)
+        self.threadID = thread_id
+
+    def run(self):
+        print("Starting " + self.name)
+        serve()
+        print("Exiting " + self.name)
+
+
+def serve():
+    if __name__ == '__main__':
+        global app
+        # wrap Flask application with engineio's middleware
+        app = socketio.Middleware(sio, app)
+
+        # deploy as an eventlet WSGI server
+        eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
 
 
 @app.route('/')
 def index():
-    """Serve the client-side application."""
-    print("Welcome")
-    return 'index.html'
+    # Serve the client-side application
+    users = len(sid_array)
+    return "There is " + str(users) + " users."
 
 
 @sio.on('connect')
 def connect(sid, environ):
-    print("connect ", sid)
+    sid_array.append(sid)
+    users = len(sid_array)
+    print("There is " + str(users) + " users.")
 
 
 @sio.on('serverStatusUpdate')
@@ -42,12 +72,37 @@ def message(sid, data):
 
 @sio.on('disconnect')
 def disconnect(sid):
-    print('disconnect ', sid)
+    sid_array.remove(sid)
+    users = len(sid_array)
+    print("There is " + str(users) + " users left.")
+    print('Disconnected')
 
 
-if __name__ == '__main__':
-    # wrap Flask application with engineio's middleware
-    app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+server_thread = Server("Server-thread")
+server_thread.start()
+threads.append(server_thread)
+# while True:
+#     print("STart of while")
+#     gcm = GCM(API_KEY)
+#     data = {'message': 'Hello from python!'}
+#
+#     # Downstream message using JSON request
+#     reg_ids = ['']
+#     response = gcm.json_request(registration_ids=reg_ids, data=data)
+#
+#     # Successfully handled registration_ids
+#     if response and 'success' in response:
+#         for reg_id, success_id in response['success'].items():
+#             print('Successfully sent notification for reg_id {0}'.format(reg_id))
+#
+#     # Handling errors
+#     if 'errors' in response:
+#         for error, reg_ids in response['errors'].items():
+#             # Check for errors and act accordingly
+#             if error in ['NotRegistered', 'InvalidRegistration']:
+#                 # Remove reg_ids from database
+#                 for reg_id in reg_ids:
+#                     print("Removing reg_id: {0} from db".format(reg_id))
+#
+#     print("End of while")
+#     time.sleep(50)
