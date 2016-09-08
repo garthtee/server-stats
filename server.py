723e35bf534e030c3dbb33aa4e97d85c59073345
Garth Toland <tolandgarth@gmail.com>
2016-09-08 14:19:26 +0100

    Send Message method created, alerts on core0 exceeding 60 degrees.

--- server.py
+++ server.py
@@ -8,15 +8,15 @@ import threading
 from flask import Flask, render_template
 from gcm import GCM
 
-
 sio = socketio.Server()
 app = Flask(__name__)
 sid_array = []
 threads = []
-API_KEY = ''
+API_KEY = ''  # Google API Key
+reg_ids = []  # Registration Ids
 
 
-class Server (threading.Thread):
+class Server(threading.Thread):
     def __init__(self, thread_id):
         threading.Thread.__init__(self)
         self.threadID = thread_id
@@ -35,6 +35,30 @@ def serve():
 
         # deploy as an eventlet WSGI server
         eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+
+
+def send_message(reg_ids):
+    gcm = GCM(API_KEY)
+    data = {'message': 'Hello from python!'}
+
+    # Downstream message using JSON request
+    response = gcm.json_request(registration_ids=reg_ids, data=data)
+
+    # Successfully handled registration_ids
+    if 'success' in response:
+        print(response)
+        for reg_id, success_id in response['success'].items():
+            print('Successfully sent notification for reg_id {0}'.format(reg_id))
+
+    # Handling errors
+    if 'errors' in response:
+        for error, reg_ids in response['errors'].items():
+            print("Unsuccessful. ERROR = " + str(response['errors']))
+            # Check for errors and act accordingly
+            if error in ['NotRegistered', 'InvalidRegistration']:
+                # Remove reg_ids from database
+                for reg_id in reg_ids:
+                    print("Removing reg_id: {0} from db".format(reg_id))
 
 
 @app.route('/')
@@ -57,11 +81,16 @@ def message(sid, data):
     n = json.dumps(data)
     json_data = json.loads(n)
     core0 = json_data.partition("core0': u'")[-1].rpartition("', u'core1")[0]
+    core0 = int(core0)
     core1 = json_data.partition("core1': u'")[-1].rpartition("'")[0]
+    core1 = int(core1)
     print("*********")
-    print(core0)
-    print(core1)
+    print(str(core0))
+    print(str(core1))
     print("*********")
+    if core0 > 60:
+        print("It is greater than 60!")
+        send_message(reg_ids)
 
 
 @sio.on('chat message')
@@ -78,31 +107,8 @@ def disconnect(sid):
     print('Disconnected')
 
 
+# ////////////////// Main Program //////////////////////// #
+
 server_thread = Server("Server-thread")
 server_thread.start()
 threads.append(server_thread)
-# while True:
-#     print("STart of while")
-#     gcm = GCM(API_KEY)
-#     data = {'message': 'Hello from python!'}
-#
-#     # Downstream message using JSON request
-#     reg_ids = ['']
-#     response = gcm.json_request(registration_ids=reg_ids, data=data)
-#
-#     # Successfully handled registration_ids
-#     if response and 'success' in response:
-#         for reg_id, success_id in response['success'].items():
-#             print('Successfully sent notification for reg_id {0}'.format(reg_id))
-#
-#     # Handling errors
-#     if 'errors' in response:
-#         for error, reg_ids in response['errors'].items():
-#             # Check for errors and act accordingly
-#             if error in ['NotRegistered', 'InvalidRegistration']:
-#                 # Remove reg_ids from database
-#                 for reg_id in reg_ids:
-#                     print("Removing reg_id: {0} from db".format(reg_id))
-#
-#     print("End of while")
-#     time.sleep(50)
